2e6535b553984eff097d89fab50f1a81e750f8bf
华哥 <20143720969@qq.com>
2021-11-15 16:34:55 +0800

    提交代码

---
+++ 主文档.c
@@ -0,0 +1,102 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+int main()
+{
+
+
+
+
+
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//my_strlen(char*num)
+//{
+//	char*star = num;
+//	char*end = num;
+//	while (*end!= '\0')
+//	{
+//		end+=1;
+//	}
+//	return (end) - (star);
+//
+//
+//}
+//int main()
+//{
+//	int len=0;
+//	char str[9] = "lizhhua";
+//	len = my_strlen(str);
+//	printf("%d",len);
+//	
+//	
+//	return 0;  
+//}
+	/*
+	char a = 0xb6;
+	short b = 0xb600;
+	int c = 0xb6000000;
+	if (a == 0xb6)
+		printf("a");
+	if (b == 0xb600)
+		printf("b");
+	if (c == 0xb6000000)
+		printf("c");
+	return 0;*/
+
+//struct stu
+//{
+//	char name[20];
+//	int age;
+//	char id[20];
+//
+//};
+//int main()
+//{
+//	struct stu s = { "liuzhenhua",18,"1234564"     };
+//	struct stu*m = &s;
+//	printf( "%s\n",s.id);
+//	printf("%d\n",(*m).age);
+//	printf("%s\n", m->name);
+//	/*int a = -1;
+//	int b = a >> 11;
+//	printf("%d", b);
+//*/
+//
+//
+//	return 0;
+//}
+
+	
+	 
+
+
+
